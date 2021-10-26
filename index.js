a3395fd5483d359088074f7d58c160831011f0b3
doctone <samjojames@gmail.com>
2021-10-26 13:53:14 +0100

    re-ordered cards

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const buttons = document.querySelectorAll('.get-data');
 const URL1 = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?camera="
 const URL2 = "&api_key=OYQjVDhF8oF9FhK1RdxSwLeiWaA9xox0GFsNvEW1"
-// const working = ""https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?camera=FHAZ&api_key=OYQjVDhF8oF9FhK1RdxSwLeiWaA9xox0GFsNvEW1""
 const results = document.querySelector('.results')
-// const correctURL = `${URL1}${button.id}${URL2}`
+
 buttons.forEach(button => {
-    button.addEventListener('click', (event) => {
+    button.addEventListener('click', () => {
         fetch(`${URL1}${button.id}${URL2}`)
         .then(res => res.json(`${URL1}${button.id}${URL2}`))
         .then(data => {
@@ -23,8 +22,8 @@ function showData(data){
         <a href="${photo.img_src}"><img src="${photo.img_src}" class="card-img-top"></a>
             <div class="card-body">
                 <h5 class="card-title">${photo.camera.full_name}</h5>
-                <p class="card-text">Photo taken: ${photo.earth_date}.</p>
                 <p>status: ${photo.rover.status}</p>
+                <p class="card-text">Photo taken: ${photo.earth_date}.</p>
                 <p> sol: ${photo.sol} </p>
                 <a href="#" class="btn btn-dark">Download</a>
             </div>
